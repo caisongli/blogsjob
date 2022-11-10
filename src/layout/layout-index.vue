2474e1838e8714da79de6cfde71619c7509ea6ac
lcs <licaisong0622@163.com>
2022-11-10 11:01:21 +0800

    style('代码风格，tab缩进'): '代码风格改变，首行缩进，其余没变'

--- src/layout/layout-index.vue
+++ src/layout/layout-index.vue
@@ -1,21 +1,21 @@
 <template>
-    <el-container direction="vertical" style="height:100vh;">
-        <el-header>
-            <el-link ref="link" :underline="false" icon="el-icon-s-home" style="fontSize:20px"
-                href='https://www.lcsblogs.com'>主页</el-link>
-            <span style="fontSize:20px;fontWeight:900;">我的小后台</span>
-            <!-- &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;时间：{{ data }} -->
-            <canvas id="canva"></canvas>
-        </el-header>
-        <el-container style="height:100vh;overflow:hidden;">
-            <el-aside width="auto">
-                <aside-menu></aside-menu>
-            </el-aside>
-            <el-main>
-                <router-view></router-view>
-            </el-main>
-        </el-container>
+  <el-container direction="vertical" style="height:100vh;">
+    <el-header>
+      <el-link ref="link" :underline="false" icon="el-icon-s-home" style="fontSize:20px"
+        href='https://www.lcsblogs.com'>主页</el-link>
+      <span style="fontSize:20px;fontWeight:900;">我的小后台</span>
+      <!-- &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;时间：{{ data }} -->
+      <canvas id="canva"></canvas>
+    </el-header>
+    <el-container style="height:100vh;overflow:hidden;">
+      <el-aside width="auto">
+        <aside-menu></aside-menu>
+      </el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
     </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -23,76 +23,77 @@ import AsideMenu from './aside-menu.vue';
 import { formatDate } from '../hooks/formatDate'
 import { myCanvas } from '@/utils/canvas.js'
 export default {
-    components: { AsideMenu },
-    data() {
-        return {
-            data: formatDate(),
-            // data :new Date().getTime(),
-            timer: '',
-        }
-    },
-    filters: {
-        //    formatDates(value){
-        //       return formatDate(value)
-        //    }
-    },
-    computed: {
-        //  formatDates(){
-        //     return formatDate()
-        //  }
-    },
-    mounted() {
-        let _this = this
-        this.timer = setInterval(() => {
-            _this.data = formatDate()
-        }, 1000)
-        this.$nextTick(() => {
-            this.fns(300, 80,"canva",1.5)
-        })
+  components: { AsideMenu },
+  data() {
+    return {
+      data: formatDate(),
+      // data :new Date().getTime(),
+      timer: '',
+    }
+  },
+  filters: {
+    //    formatDates(value){
+    //       return formatDate(value)
+    //    }
+  },
+  computed: {
+    //  formatDates(){
+    //     return formatDate()
+    //  }
+  },
+  mounted() {
+    let _this = this
+    this.timer = setInterval(() => {
+      _this.data = formatDate()
+    }, 1000)
+    this.$nextTick(() => {
+      this.fns(300, 80, "canva", 1.5)
+    })
 
-    },
-    destroyed() {
-        clearInterval(this.timer)
-    },
-    watch: {
-    },
-    methods: {
-        // 
-    },
-    mixins: [myCanvas]
+  },
+  destroyed() {
+    clearInterval(this.timer)
+  },
+  watch: {
+  },
+  methods: {
+    // 
+  },
+  mixins: [myCanvas]
 }
 </script>
 
 <style lang="less" scoped>
 .el-header {
-    background-color: @theme-header;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    > canvas{
-        margin-left: 100px;
-    }
+  background-color: @theme-header;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  >canvas {
+    margin-left: 100px;
+  }
 }
 
 .el-aside {
-    background-color: @theme-aside;
-    color: #333;
-    text-align: center;
-    line-height: 100px;
+  background-color: @theme-aside;
+  color: #333;
+  text-align: center;
+  line-height: 100px;
 }
 
 .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-    // height: 800px;
-    overflow-y: auto;
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  // height: 800px;
+  overflow-y: auto;
 }
 
 body>.el-container {
-    margin-bottom: 40px;
+  margin-bottom: 40px;
 }
 </style>
